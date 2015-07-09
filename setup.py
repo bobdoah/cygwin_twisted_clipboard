e3e8af2c543d7d1986597823bc6a9a0d2f511af5
Rob Williams <robert.williams@hds.com>
2015-07-09 16:20:40 +0100

    Rename the project, it's no longer cygwin-specific

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cygwin_twisted_clipboard',
-    description="A twisted server which exposes cygwin's /dev/clipboard",
-    version='1.0',
+    name='twisted_pyperclip',
+    description="A twisted server which exposes the clipboard using the cross-platform pyperclip library",
+    version='1.1',
     author='Bob Williams',
     author_email='robertwilliams1985@gmail.com',
     packages=find_packages(),
-    url='https://github.com/bobdoah/cygwin_twisted_clipboard',
+    url='https://github.com/bobdoah/twisted_pyperclip',
     entry_points={
         'console_scripts' : [
-            'cygwin-twisted-clipboard-server = cygwin_twisted_clipboard.server:main',
+            'twisted-pyperclip-server = twisted_pyperclip.server:main',
         ]
     },
     install_requires=['Twisted>=15.2.1', 'pyperclip']
